56ed8a6286a8f8ef71f20b3d3a5f15605087fe65
ScorpioXKiller <dima.gorodov1999@gmail.com>
2025-02-01 19:49:48 +0200

    basic server setup

--- Server.py
+++ Server.py
@@ -1,29 +1,56 @@
 import socket
+import selectors
 import threading
-from Network import Network
+from ClientHandler import ClientHandler
 
-class Server(Network):
-    def __init__(self, host, port):
-        super().__init__(host, port)
-        self.client = {} # dictionary to store client socket objects. For each client, we store the client socket object in the dictionary, with the client's address as the key and the client socket object as the value.
+PORT_FILE = "myport.info"
+DEFAULT_PORT = 1357
+
+class Server:
+    def __init__(self):
+        self.port = self.get_port()
+        self.selector = selectors.DefaultSelector()
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.clients = []
+        self.running = True
+
+    def get_port(self):
+        try: 
+            with open(PORT_FILE, "r") as file:
+                port_data = file.read().strip()
+                if not port_data:
+                    print("Warning: Port file is empty. Using default port.") 
+                    return DEFAULT_PORT
+                return int(port_data) if port_data else DEFAULT_PORT
+        except (FileNotFoundError):
+            print("Warning: Port file not found. Using default port.")
+            return DEFAULT_PORT
+        except (ValueError):
+            print("Warning: Port file contains invalid data. Using default port.")
+            return DEFAULT_PORT
         
-    def handle_client(self, client_socket: socket, client_address):
-        # Handle each client connection in a separate thread
-        
-        try:
-            while True:
-                data = client_socket.socket.recv(1024).decode('utf-8')
-                if not data:
-                    break
-                
-                print(f"Received data from {client_address}: {data}")
-                
-                command, *args = data.split("|")
-                
-                if command == "send":
-                    receiver, encrypted_message = args
-        
-        except:
-            pass
-        finally:
-            client_socket.close()
+    def start(self):
+        self.server_socket.bind(("0.0.0.0", self.port))
+        self.server_socket.listen()
+        print(f"Server listening on port {self.port}")
+
+        threading.Thread(target=self.shutdown, daemon=True).start()
+
+        while self.running:
+            try:
+                client_socket, address = self.server_socket.accept()
+                client_handler = ClientHandler(client_socket, address)
+                threading.Thread(target=client_handler.handle, daemon=True).start()
+                self.clients.append(client_handler)
+                print(f"Number of clients: {len(self.clients)}")
+            except OSError as e:
+                print(f"Error accepting client: {e}")
+                break
+
+    def shutdown(self):
+        while self.running:
+            if input().strip().lower() == "q":
+                self.running = False
+                self.server_socket.close()
+                print("Server shutting down...")
+                break
